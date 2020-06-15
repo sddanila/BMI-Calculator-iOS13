a7b50336a98ff1e8d44ae8d998061bdd7d5af792
Danila Barton-Szabo <danilabarton-szabo@Enemy18s-MacBook-Pro.local>
2020-06-15 17:03:12 -0600

    add bmi IB actions and outlets for the slider and buttons as well as the initial BMI calculations

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightChange(_ sender: UISlider) {
+        let changedHeight = sender.value
+        heightLabel.text = "\(String(format: "%.2f", changedHeight)) m"
+        print(sender.value)
+    }
+    
+    @IBAction func weightChange(_ sender: UISlider) {
+        let changedWeight = sender.value
+        weightLabel.text = "\(String(format: "%.0f", changedWeight)) kg"
+        print(sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let heightValue = heightSlider.value
+        let weightValue = weightSlider.value
+        
+        let bmi = weightValue / pow(heightValue, 2)
+        
+        print(bmi)
+    }
 }
 
